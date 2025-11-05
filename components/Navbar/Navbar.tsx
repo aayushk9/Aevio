98b72f4f38f32eae02033280fba0668818cb83f6
Aayush <ayushkokate1@gmail.com>
2025-11-05 18:10:54 +0530

    chore(ui): replace a tag with next link

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,44 +1,44 @@
 "use client";
 
-import { useState } from "react";
+import Link from "next/link";
 
 export function Navbar() {
 
   return (
     <nav className="fixed inset-x-0 top-0 z-50 flex justify-center backdrop-blur-sm">
       <div className="flex w-full max-w-screen-2xl items-center justify-between px-6 py-4 md:px-8">
-        <a href="/" className="flex items-center">
+        <Link href="/" className="flex items-center">
           <span className="text-xs font-semibold uppercase tracking-widest text-white">
             Aevio
           </span>
-        </a>
+        </Link>
 
         <div className="hidden md:block">
           <ul className="flex items-center space-x-9 sp">
             <li>
-              <a
+              <Link
               href="/controller"
               className="text-xs font-semibold uppercase tracking-widest text-white transition-colors hover:text-gray-300"
               >Controller
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 href="/docs"
                 className="text-xs font-semibold uppercase tracking-widest text-white transition-colors hover:text-gray-300"
               >
                 Docs
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 href="/about"
                 className="text-xs font-semibold uppercase tracking-widest text-white transition-colors hover:text-gray-300"
               >
                 About
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
